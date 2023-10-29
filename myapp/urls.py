3b1f823c893d895887dec8ceb7b120e39e77dd41
billojango <billojango>
2023-10-29 17:18:19 +0300

    Django url added

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,9 +5,9 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('gallery/', views.gallery),
-    path('about/', views.aboutus),
-    path('contact/', views.contactus),
-    path('table/', views.tableview),
+    path('', views.home, name='home'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('about/', views.aboutus, name='about'),
+    path('contact/', views.contactus, name='contact'),
+    path('table/', views.tableview, name='table'),
 ]
